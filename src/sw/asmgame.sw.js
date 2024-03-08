7ec1b660ce6cecbe5f609850444ae7dbbcfdd888
Arthur Beck <averse.abfun@gmail.com>
2024-03-08 15:21:09 +0000

    networkFirst

--- src/sw/asmgame.sw.js
+++ src/sw/asmgame.sw.js
@@ -1,22 +1,7 @@
 const version = 'v1';
 
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open(version);
-  await cache.addAll(resources);
-};
-
 self.addEventListener('install', (event) => {
-  console.log(`${version} installing...`);
-
-  event.waitUntil(
-    addResourcesToCache([
-      'index.html',
-      'style.css',
-      'main.js',
-      'puzzles.json',
-      'sw-register.js'
-    ])
-  );
+  console.log(`${version} installed`);
 });
 
 const putInCache = async (request, response) => {
@@ -30,20 +15,20 @@ const putInCache = async (request, response) => {
     await cache.put(request, response);
 };
   
-const cacheFirst = async (request) => {
-    const responseFromCache = await caches.match(request);
-  
-    if (responseFromCache) {
-      return responseFromCache;
-    }
-  
+const networkFirst = async (request) => {
     const responseFromNetwork = await fetch(request);
-  
+
     putInCache(request, responseFromNetwork.clone());
   
-    return responseFromNetwork;
+    if (responseFromNetwork) {
+      return responseFromNetwork;
+    }
+  
+    const responseFromCache = await caches.match(request);
+  
+    return responseFromCache;
 };
   
 self.addEventListener('fetch', (event) => {
-    event.respondWith(cacheFirst(event.request));
+    event.respondWith(networkFirst(event.request));
 });
