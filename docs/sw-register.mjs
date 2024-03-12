9a194b9b3e21744ceb839a1e527a8af5f4f59e9f
Arthur Beck <averse.abfun@gmail.com>
2024-03-12 11:56:10 +0000

    better icons but they still aren't great

--- docs/sw-register.mjs
+++ docs/sw-register.mjs
@@ -1,7 +1,7 @@
 const registerWorker = async () => {
     if ('serviceWorker' in navigator) {
       try {
-        const registration = await navigator.serviceWorker.register('asmgame.sw.js');
+        const registration = await navigator.serviceWorker.register('sw/asmgame.sw.js');
         console.log('Service worker registration succeeded:', registration);
       } catch (error) {
         alert(`Service Worker Registration failed with ${error}`);
